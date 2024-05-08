e62706988f3a35b0c7424ea4af0cb6502cdd249c
Brian Carmichael <bdonjr7@gmail.com>
2024-05-07 20:45:55 -0700

    new migration

---
+++ migrations/versions/20240507_164945_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 9dfadb541de0
+Revises: 9dfadb541de0
+Create Date: 2024-05-07 16:49:45.568262
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9dfadb541de0'
+down_revision = '9dfadb541de0'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
